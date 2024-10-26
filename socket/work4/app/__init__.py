76812ce186a98080f5c853dcb4492da188fc4cfa
QING <qlear@qq.com>
2024-10-26 18:18:40 +0800

    chore(work4): 整理了项目结构

--- socket/work4/app/__init__.py
+++ socket/work4/app/__init__.py
@@ -8,7 +8,9 @@ logging.basicConfig(level=logging.INFO, format="[%(asctime)s] - %(message)s")
 class Config:
     HOST = "127.0.0.1"
     PORT = 9999
-    WEB_ROOT = os.path.join(os.path.dirname(__file__), "html")
+    WEB_ROOT = os.path.join(
+        os.path.abspath(os.path.join(os.path.dirname(__file__), "..")), "html"
+    )
     SERVER_NAME = "Seeker dev"
     DEFAULT_INDEX = "index.html"
 
